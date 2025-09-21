2c2fa6e3a99eb7324fbf045ef68ab8442ed1e954
ozanzadeoglu <ozanzadeoglu01@gmail.com>
2025-09-22 02:01:20 +0300

    Add updata and delete methods to service

--- todos/services.py
+++ todos/services.py
@@ -1,7 +1,34 @@
 from .models import Todo
 from django.db import transaction
 
+
 @transaction.atomic
 def todo_create(*, title: str, description: str, completed: bool = False) -> Todo:
-    todo = Todo.objects.create(title = title, description = description, completed = completed)
+    todo = Todo.objects.create(
+        title=title, description=description, completed=completed
+    )
     return todo
+
+
+@transaction.atomic
+def todo_update(*, todo: Todo, title = None, description = None, completed = None) -> Todo:
+    #need to check generic solutions
+    if title is not None:
+        todo.title = title
+    if description is not None:
+        todo.description = description
+    if completed is not None:
+        todo.completed = completed
+
+    todo.save()
+    return todo
+
+
+@transaction.atomic
+def todo_delete(*, todo_pk: int) -> bool:
+    deleted_count, _ = Todo.objects.filter(pk=todo_pk).delete()
+
+    if deleted_count == 0:
+        return False
+
+    return True
